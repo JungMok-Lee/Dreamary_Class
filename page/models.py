af27f67c0bc8e09e9d5c2d252835cbc6baa0d15e
JungMok-Lee <gracelee1020@naver.com>
2020-08-24 20:29:05 +0900

    third maybe

--- page/models.py
+++ page/models.py
@@ -1,7 +1,4 @@
-#-*- coding:utf-8 -*-
-
-from django.db import models
-
+from django.db import models\
 class Designer(models.Model):
     image=models.ImageField(upload_to='images/')
     name=models.CharField(max_length=50)
